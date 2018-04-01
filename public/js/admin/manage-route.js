7b628f02cfd0358753b49a8ba0ffe9192231a664
patcharapol1234 <37959446+patcharapol1234@users.noreply.github.com>
2018-04-02 00:17:45 +0700

    updatecode
    updatecode

---
+++ public/js/admin/manage-route.js
@@ -0,0 +1,25 @@
+$(function () {
+    $(document).ready(function() {
+        alert('sdasd');
+       /* $('#example').DataTable( {
+            "processing": true,
+            "serverSide": true,
+            "ajax": {
+                "url": "scripts/post.php",
+                "type": "POST"
+            },
+            "columns": [
+                { "data": "first_name" },
+                { "data": "last_name" },
+                { "data": "position" },
+                { "data": "office" },
+                { "data": "start_date" },
+                { "data": "salary" }
+            ]
+        } );*/
+    } );
+});
+
+
+
+
