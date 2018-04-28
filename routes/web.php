ef95ffe820866c4f47392c610e2984536a929102
champ <champ6803@gmail.com>
2018-04-29 00:06:20 +0700

    commit

--- routes/web.php
+++ routes/web.php
@@ -115,9 +115,8 @@ Route::get('contact','ContactController@contact_us');
 
 Route::get('hothits','HothitsController@hot_hits');
 
-<<<<<<< HEAD
 Route::get('logout','LoginController@logout');
-=======
+
 Route::get('manage-front-country','ManageFrontController@manage_front_country');
 
 Route::post('searchAllCountry', 'ManageFrontController@searchAllCountry');
@@ -131,5 +130,3 @@ Route::post('deleteTourCountry', 'ManageFrontController@deleteTourCountry');
 Route::post('updateTourCountry', 'ManageFrontController@updateTourCountry');
 
 Route::get('admin','AdminLoginController@login_admin');
-
->>>>>>> b09d047177b0bfc9ac903c2d8b2923236a05f3b3
