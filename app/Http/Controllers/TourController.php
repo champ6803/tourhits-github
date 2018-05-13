88bb1c7513c1c04b5329d25544038063a3fb6acc
champ <champ6803@gmail.com>
2018-05-13 11:24:22 +0700

    tour package update

--- app/Http/Controllers/TourController.php
+++ app/Http/Controllers/TourController.php
@@ -11,27 +11,29 @@ namespace App\Http\Controllers;
 use App\Models\Tour_Package;
 use App\Models\Tour_Package_Day;
 
-/**x
+/* * x
  * Description of TourController
  *
  * @author chonl
  */
+
 class TourController {
 
-    public function tour_detail($tour_countr_name,$tour_package_id, $tour_package_name) {
+    public function tour_detail($tour_countr_name, $tour_package_id, $tour_package_name) {
         $tourModel = new Tour_Package();
         $tourDayModel = new Tour_Package_Day();
         $tourPackage = $tourModel->getTourDetail($tour_package_id);
         $tourPackageList = $tourModel->getTourDetailList($tour_package_id);
         $tourPackageDayList = $tourDayModel->getTourPackageDay($tour_package_id);
-        foreach ($tourPackageList as $tourPackageObj){
+        $tourPackageImagesList = $tourModel->getTourImages($tour_package_id);
+        foreach ($tourPackageList as $tourPackageObj) {
             $newStartDate = date("d-m-Y", strtotime($tourPackageObj->tour_period_start));
             $newEndDate = date("d-m-Y", strtotime($tourPackageObj->tour_period_end));
             $tourPackageObj->tour_period_start = $newStartDate;
             $tourPackageObj->tour_period_end = $newEndDate;
         }
         $page_title = $tourPackage->tour_package_name;
-        return view('tour.tour-detail', compact('tourPackage', 'tourPackageList','tourPackageDayList', 'page_title'));
+        return view('tour.tour-detail', compact('tourPackage', 'tourPackageList', 'tourPackageImagesList', 'tourPackageDayList', 'page_title'));
     }
-}
 
+}
