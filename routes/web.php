807167c7500be40f13132af64b559c0db47afcf3
champ <champ6803@gmail.com>
2018-06-18 01:08:29 +0700

    up

--- routes/web.php
+++ routes/web.php
@@ -159,8 +159,6 @@ Route::get('order-list', 'DashboardController@order_list');
 
 Route::post('searchAllAttraction', 'AdminController@searchAllAttraction');
 
-<<<<<<< HEAD
 Route::post('searchAllTag', 'AdminController@searchAllTag');
-=======
+
 Route::post('searchAllTag', 'AdminController@searchAllTag');
->>>>>>> b3c9ee4435ca81b4e81be8cf279c43ab49c22822
