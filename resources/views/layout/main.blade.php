636e43200222880553fba2dffea8e90abc853de7
chatchawan <champ6803@gmail.com>
2019-02-02 13:08:22 +0700

    Update main.blade.php

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -12,6 +12,7 @@ session_start();
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
         <meta name="format-detection" content="telephone=no">
         <meta name="apple-mobile-web-app-capable" content="yes">
+        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
         <!--CSRF Token-->
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
